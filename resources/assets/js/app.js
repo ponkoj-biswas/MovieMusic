a248ef85cff2ac1546a87c307c816c4b74c8f1a9
ponkoj-biswas <ponkoj.cse@gmail.com>
2018-02-08 11:14:22 +0600

    configure css and js for our app and configure database

---
+++ resources/assets/js/app.js
@@ -0,0 +1,7 @@
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+import Buefy from 'buefy';
+vue.use(Buefy);
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
